98233bf3ee5b6ce27ec804aab53feea23b9042b0
163432591 <73006032+163432591@users.noreply.github.com>
2020-10-21 17:16:47 +0600

    Add files via upload

---
+++ problem set 1/4.c
@@ -0,0 +1,28 @@
+
+#include<stdio.h>
+#include <float.h>
+
+int main(){
+
+int a;
+float b =1.0;
+char c;
+double d;
+long double e;
+long int f;
+short int g;
+long long int h;
+unsigned int i;
+unsigned short int j;
+
+
+printf("the memory location of int is: %p\n",&a);
+printf("the memory location of float is:%p\n",&b);
+printf("the memory location of char is:%p\n",&c);
+printf("the memory location of double is:%p\n",&d);
+printf("the memory location of long double is:%p\n",&e);
+printf("the memory location of long int is:%p\n",&f);
+printf("the memory location of short int is:%p\n",&g);
+printf("the memory location of long long int is:%p\n",&h);
+printf("the memory location of unsigned int is:%p\n",&i);
+printf("the memory location of unsigned short int  is:%p",&j);
