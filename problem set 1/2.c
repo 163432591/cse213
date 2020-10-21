e5134fa8d87945d3e2ffac4763f4e2be06ec5b6c
163432591 <73006032+163432591@users.noreply.github.com>
2020-10-21 17:20:49 +0600

    Add files via upload

---
+++ problem set 1/2.c
@@ -0,0 +1,14 @@
+
+#include<stdio.h>
+int main() {
+    float  a;
+    double b;
+    long double c;
+
+    printf("Size of float : %ld bytes\n", sizeof(a));
+    printf("Size of double: %ld bytes\n", sizeof(b));
+    printf("Size of long double: %ld bytes\n", sizeof(c));
+    return 0;
+
+
+}
