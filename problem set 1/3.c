8c79a2c6fa5e3470eb2b3e4ee92b72ee4190e70f
163432591 <73006032+163432591@users.noreply.github.com>
2020-10-21 17:16:20 +0600

    Add files via upload

---
+++ problem set 1/3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <float.h>
+
+int main () {
+   printf("The maximum value of float = %.10e\n", FLT_MAX);
+   printf("The minimum value of float = %.10e\n", FLT_MIN);
+
+   printf("The maximum value of double = %.10e\n", DBL_MAX);
+   printf("The minimum value of double  = %.10e\n", DBL_MIN);
+
+
+}
+
